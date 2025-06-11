c3203d824116386361acf138c4c3c253c61ac66e
salah929 <salah.sy@msn.com>
2025-06-11 02:09:57 +0200

    Add code for the deploy

--- eventease/settings.py
+++ eventease/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=pmp#skx=xtu%b^r-9p+wt@j3)&49b&)s+3s13%+1qi@0p)(6x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
