4b3fc574a14177ae0c3a07c602103ebbccbab222
salah929 <salah.sy@msn.com>
2025-06-12 02:03:57 +0200

    Add Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+STATUS = ((0, "Pending"), (1, "Approved"))
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                   related_name='created_events')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
